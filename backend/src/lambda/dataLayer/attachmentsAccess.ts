6a5d8bb9e59c9fd64a360331a91448d22cfb8906
Hanna Becker <hanna.e.becker@gmail.com>
2020-05-05 00:19:12 +0200

    do not use x-ray for local execution

--- backend/src/lambda/dataLayer/attachmentsAccess.ts
+++ backend/src/lambda/dataLayer/attachmentsAccess.ts
@@ -1,14 +1,14 @@
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
-
-const XAWS = AWSXRay.captureAWS(AWS);
+// import * as AWSXRay from 'aws-xray-sdk'
+//
+// const XAWS = AWSXRay.captureAWS(AWS);
 
 const bucketName = process.env.IMAGES_S3_BUCKET;
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION;
 
 export class AttachmentsAccess {
 
-    constructor(private readonly s3 = new XAWS.S3({signatureVersion: 'v4'})) {
+    constructor(private readonly s3 = new AWS.S3({signatureVersion: 'v4'})) {
     }
 
     async deleteAttachment(attachmentId: string): Promise<void> {
--- backend/src/lambda/dataLayer/todoItemsAccess.ts
+++ backend/src/lambda/dataLayer/todoItemsAccess.ts
@@ -1,10 +1,10 @@
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
 import {DocumentClient} from 'aws-sdk/clients/dynamodb'
 import {TodoItem} from "../../models/TodoItem";
 import {UpdateTodoRequest} from "../../requests/UpdateTodoRequest";
-
-const XAWS = AWSXRay.captureAWS(AWS);
+// import * as AWSXRay from 'aws-xray-sdk'
+//
+// const XAWS = AWSXRay.captureAWS(AWS);
 
 export class TodoItemsAccess {
 
@@ -117,11 +117,11 @@ export class TodoItemsAccess {
 function createDynamoDBClient() {
     if (process.env.IS_OFFLINE) {
         console.log('Creating a local DynamoDB instance');
-        return new XAWS.DynamoDB.DocumentClient({
+        return new AWS.DynamoDB.DocumentClient({
             region: 'localhost',
             endpoint: 'http://localhost:8000'
         })
     }
 
-    return new XAWS.DynamoDB.DocumentClient();
+    return new AWS.DynamoDB.DocumentClient();
 }
