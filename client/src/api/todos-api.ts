557e229939955678cbc57399898268f7fd6a19ab
Hanna Becker <hanna.e.becker@gmail.com>
2020-05-05 07:17:56 +0200

    implement delete song (frontend)

--- client/src/api/todos-api.ts
+++ client/src/api/todos-api.ts
@@ -33,6 +33,7 @@ export async function createTodo(
 
 // TODO: prevent adding the same song twice
 export async function addSong(idToken: string, trackId: string): Promise<string> {
+  console.log('Adding song')
   const response = await Axios.post(`${apiEndpoint}/songs`, JSON.stringify({ trackId }), {
     headers: {
       'Content-Type': 'application/json',
@@ -43,6 +44,7 @@ export async function addSong(idToken: string, trackId: string): Promise<string>
 }
 
 export async function deleteSong(idToken: string, trackId: string): Promise<void> {
+  console.log('Deleting song')
   await Axios.delete(`${apiEndpoint}/songs/${trackId}`, {
     headers: {
       'Content-Type': 'application/json',
@@ -53,14 +55,12 @@ export async function deleteSong(idToken: string, trackId: string): Promise<void
 
 export async function searchSong(idToken: string, searchTerm: string): Promise<SearchResult[]> {
   console.log('Searching for songs')
-
   const response = await Axios.get(`${apiEndpoint}/searchSong?searchTerm=${searchTerm}`, {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${idToken}`
     }
   })
-  console.log('Search results:', response.data)
   return response.data
 }
 
--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -2,7 +2,7 @@ import dateFormat from 'dateformat'
 import { History } from 'history'
 import * as React from 'react'
 import { Button, Divider, Grid, Header, Icon, Image, Input, Loader } from 'semantic-ui-react'
-import { addSong, createTodo, getTodos, searchSong } from '../api/todos-api'
+import { addSong, createTodo, deleteSong, getTodos, searchSong } from '../api/todos-api'
 import Auth from '../auth/Auth'
 import { Todo } from '../types/Todo'
 import { SearchResult } from '../types/SearchResult'
@@ -74,15 +74,14 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
   }
 
   onSongDelete = async (trackId: string) => {
-    // TODO: implement
-    // try {
-    //   await deleteTodo(this.props.auth.getIdToken(), trackId)
-    //   this.setState({
-    //     todos: this.state.todos.filter(todo => todo.todoId != trackId)
-    //   })
-    // } catch {
-    //   alert('Todo deletion failed')
-    // }
+    try {
+      await deleteSong(this.props.auth.getIdToken(), trackId)
+      this.setState({
+        trackIds: this.state.trackIds.filter(id => id != trackId)
+      })
+    } catch {
+      alert('Todo deletion failed')
+    }
   }
 
   onSongAdd = async (trackId: string) => {
